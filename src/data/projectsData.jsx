b1e2adc32a1803cdfb4922c6b8c6ecb123193a35
Gloria Kirivita <137270735+gloriaakr@users.noreply.github.com>
2026-01-21 00:01:31 +0400

    Fix live link path casing in projectsData

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -18,7 +18,7 @@ const projects = [
   image: frog,
   gif: null,
   links: {
-    live: /first-game,
+    live: /First-Game,
     source: null,
     case: "/projects/GodotGame2D",
     source: "https://github.com/gloriaakr/portfolioo",
